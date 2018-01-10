afd1c4780bbc5430365efda1a36da6a441ab2110
pelillian <plillian@usc.edu>
2018-01-09 20:17:41 -0800

    Revert "changed how data is loaded"
    This reverts commit b458092af7c2591b95dfa145db2c510d103fdcf8.

--- load_data.py
+++ load_data.py
@@ -15,3 +15,11 @@ def load():
 	x_train, x_test, y_train, y_test = pkl.load(f)
 	f.close()
 	return x_train, x_test, y_train, y_test
+
+def get_train():
+	x_train, x_test, y_train, y_test = load()
+	return [x_train, y_train]
+
+def get_test():
+	x_train, x_test, y_train, y_test = load()
+	return [x_test, y_test]
