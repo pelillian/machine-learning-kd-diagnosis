97b7e1a44db813cd375f4832146f73c2dbff21c3
pelillian <plillian@usc.edu>
2018-01-11 14:44:19 -0800

    finished one-hot encoding and missing data check

--- load_data.py
+++ load_data.py
@@ -10,15 +10,12 @@
 
 import pickle as pkl
 import numpy as np
+import numpy.ma as ma
 
 # fill all cells containing NaN with the average value
 # this is a basic way of dealing with missing data
 def fill_nan(data):
-	print(data)
-	column_avgs = np.nanmean(data, axis=0)
-	inds = np.where(np.isnan(data))
-	data[inds] = np.take(column_avgs, inds[1])
-	print(data)
+	data = np.where(np.isnan(data), ma.array(data, mask=np.isnan(data)).mean(axis=0), data)
 	return data
 
 # load the dataset from pkl file
@@ -26,4 +23,9 @@ def load():
 	f = open('data/kd_dataset.pkl','rb')
 	x_train, x_test, y_train, y_test = pkl.load(f)
 	f.close()
+	
+	# one-hot encode y
+	y_train = np.eye(np.max(y_train) + 1)[y_train]
+	y_test = np.eye(np.max(y_test) + 1)[y_test]
+
 	return fill_nan(x_train), fill_nan(x_test), fill_nan(y_train), fill_nan(y_test)
