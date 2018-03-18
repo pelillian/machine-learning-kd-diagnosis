e954cf596523fae9b9ecdf1d9be5c11ec3d7dd77
pelillian <plillian@usc.edu>
2018-03-18 00:36:07 -0700

    added compute_stats to run_all

--- run_all.py
+++ run_all.py
@@ -8,8 +8,20 @@
 # by Peter Lillian
 # -------------------------------------------------------------------------------------------------
 
+import numpy as np
+
 from deepnet.deep_model import DeepKDModel
 from preprocess import load_data
+
+def compute_stats(y_test, y_pred):
+	y_results = np.column_stack((y_test[:, 1], y_pred))
+
+	dt = np.dtype((np.void, y_results.dtype.itemsize * y_results.shape[1]))
+	b = np.ascontiguousarray(y_results).view(dt)
+	_, cnt = np.unique(b, return_counts=True)
+
+	return cnt
+
 
 # load data
 x_train, x_test, y_train, y_test = load_data.load(one_hot=True, fill_mode='mean')
@@ -17,4 +29,5 @@ x_train, x_test, y_train, y_test = load_data.load(one_hot=True, fill_mode='mean'
 # test deepmodel
 deep = DeepKDModel()
 deep.train(x_train, y_train)
-print(deep.test(x_test, y_test))
+deep_y_pred = deep.test(x_test, y_test)
+
