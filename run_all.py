37f7429865f9dbba79fd37a5d99070f69f6de1fd
pelillian <plillian@usc.edu>
2018-03-18 01:11:02 -0700

    updated totals

--- run_all.py
+++ run_all.py
@@ -13,14 +13,17 @@ import numpy as np
 from deepnet.deep_model import DeepKDModel
 from preprocess import load_data
 
-def compute_stats(y_test, y_pred):
+def compute_stats(y_pred, y_test):
 	y_results = np.column_stack((y_test[:, 1], y_pred))
+	y_arr = np.dtype((np.void, y_results.dtype.itemsize * y_results.shape[1]))
+	contigview = np.ascontiguousarray(y_results).view(y_arr)
+	return np.unique(contigview, return_counts=True)[1]
 
-	dt = np.dtype((np.void, y_results.dtype.itemsize * y_results.shape[1]))
-	b = np.ascontiguousarray(y_results).view(dt)
-	_, cnt = np.unique(b, return_counts=True)
-
-	return cnt
+def explain_stats(stats):
+	print("FC Classified as FC: " + str(stats[0]))
+	print("FC Classified as KD: " + str(stats[1]))
+	print("KD Classified as FC: " + str(stats[2]))
+	print("KD Classified as KD: " + str(stats[3]))
 
 
 # load data
@@ -30,4 +33,6 @@ x_train, x_test, y_train, y_test = load_data.load(one_hot=True, fill_mode='mean'
 deep = DeepKDModel()
 deep.train(x_train, y_train)
 deep_y_pred = deep.test(x_test, y_test)
+deep_stats = compute_stats(deep_y_pred, y_test)
 
+explain_stats(deep_stats)
