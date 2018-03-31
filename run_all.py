ea5821de4e9a2d29cf8d6a94af1216c358463e77
pelillian <plillian@usc.edu>
2018-03-31 15:57:14 -0700

    k-fold completed

--- run_all.py
+++ run_all.py
@@ -10,6 +10,8 @@
 
 import numpy as np
 
+from sklearn.model_selection import StratifiedKFold
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.gaussian_process import GaussianProcessClassifier
@@ -43,7 +45,7 @@ def compute_stats(y_pred, y_test):
 		y_results = np.column_stack((y_test, y_pred))
 	y_arr = np.dtype((np.void, y_results.dtype.itemsize * y_results.shape[1]))
 	contigview = np.ascontiguousarray(y_results).view(y_arr)
-	return np.unique(contigview, return_counts=True)[1]
+	return np.unique(contigview, return_counts=True)[1].tolist()
 
 # Explain TN, FP, FN, TP
 def explain_stats(stats):
@@ -59,48 +61,52 @@ def explain_stats(stats):
 	print("KD Classified as KD: " + str(stats[3]) + ", (" + str(kd_as_kd) + " %)")
 
 # Train and evaluate model, print out results
-def test_model(model, x_train, x_test, y_train, y_test):
-	y_pred = model.train_test(x_train, x_test, y_train, y_test)
-	stats = compute_stats(y_pred, y_test)
-	explain_stats(stats)
+def test_model(model, x, y):
+	stats_arr = []
+	kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=90007)
+	for train_idx, test_idx in kf.split(x, y):
+		x_train, x_test, y_train, y_test = x[train_idx], x[test_idx], y[train_idx], y[test_idx]
+		y_pred = model.train_test(x_train, x_test, y_train, y_test)
+		stats_arr.append(compute_stats(y_pred, y_test))
+	# print(str(stats_arr).encode("utf-8").decode("ascii"))
+	# print(np.mean(stats_arr, axis=0))
+	explain_stats(np.mean(stats_arr, axis=0))
 
 
-# load data one hot
-x_train, x_test, y_train, y_test = load_data.load(one_hot=True, fill_mode='mean')
+# load data
+x_train, x_test, y_train, y_test = load_data.load(one_hot=False, fill_mode='mean')
+x, y = np.concatenate((x_train, x_test)), np.concatenate((y_train, y_test))
 
 print("Our Models:")
 print("Deep Model")
-test_model(DeepKDModel(), x_train, x_test, y_train, y_test)
-
-# load data not one hot
-x_train, x_test, y_train, y_test = load_data.load(one_hot=False, fill_mode='mean')
+test_model(DeepKDModel(), x, y)
 
 print("XGBoost Model")
-test_model(XGBoostKDModel(), x_train, x_test, y_train, y_test)
+test_model(XGBoostKDModel(), x, y)
 
 print("")
 print("Scikit Models:")
 
 print("Logistic Regression")
-test_model(ScikitModel(LogisticRegression()), x_train, x_test, y_train, y_test)
+test_model(ScikitModel(LogisticRegression()), x, y)
 
 print("Support Vector Classification")
-test_model(ScikitModel(SVC()), x_train, x_test, y_train, y_test)
+test_model(ScikitModel(SVC()), x, y)
 
 print("Gaussian Process Classifier")
-test_model(ScikitModel(GaussianProcessClassifier(1.0 * RBF(1.0))), x_train, x_test, y_train, y_test)
+test_model(ScikitModel(GaussianProcessClassifier(1.0 * RBF(1.0))), x, y)
 
 print("Random Forest")
-test_model(ScikitModel(RandomForestClassifier()), x_train, x_test, y_train, y_test)
+test_model(ScikitModel(RandomForestClassifier()), x, y)
 
 print("AdaBoost Classifier")
-test_model(ScikitModel(AdaBoostClassifier()), x_train, x_test, y_train, y_test)
+test_model(ScikitModel(AdaBoostClassifier()), x, y)
 
 print("Multi-layer Perceptron")
-test_model(ScikitModel(MLPClassifier(max_iter=400)), x_train, x_test, y_train, y_test)
+test_model(ScikitModel(MLPClassifier(max_iter=400)), x, y)
 
 print("K Nearest Neighbors")
-test_model(ScikitModel(KNeighborsClassifier(4)), x_train, x_test, y_train, y_test)
+test_model(ScikitModel(KNeighborsClassifier(4)), x, y)
 
 print("Gaussian Native Bayes")
-test_model(ScikitModel(GaussianNB()), x_train, x_test, y_train, y_test)
+test_model(ScikitModel(GaussianNB()), x, y)
