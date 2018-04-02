21da4553072c20e9c3fcc75a8c4752eca7b1eff8
pelillian <plillian@usc.edu>
2018-04-01 23:14:27 -0700

    hyperparam optimization

--- run_all.py
+++ run_all.py
@@ -11,15 +11,13 @@
 import numpy as np
 
 from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import GridSearchCV
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
-from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.gaussian_process.kernels import RBF
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
 
 
 from deepnet.deep_model import DeepKDModel
@@ -28,13 +26,16 @@ from xgbst.xgboost_model import XGBoostKDModel
 from preprocess import load_data
 
 class ScikitModel:
-	def __init__(self, model, verbose=False):
-		self.model = model
+	def __init__(self, skmodel, params, verbose=False):
+		self.skmodel = skmodel
+		self.paramsearch = GridSearchCV(self.skmodel, params, cv=5)
 		self.verbose = verbose
 
 	def train_test(self, x_train, x_test, y_train, y_test):
-		self.model.fit(x_train, y_train)
-		y_pred = self.model.predict(x_test)
+		params = self.skmodel.get_params(deep=True)
+		print(params)
+		self.paramsearch.fit(x_train, y_train)
+		y_pred = self.paramsearch.predict(x_test)
 		return y_pred
 
 # Explain TN, FP, FN, TP
@@ -68,8 +69,7 @@ def test_model(model, x, y):
 		x_train, x_test, y_train, y_test = x[train_idx], x[test_idx], y[train_idx], y[test_idx]
 		y_pred = model.train_test(x_train, x_test, y_train, y_test)
 		stats_arr.append(compute_stats(y_pred, y_test))
-	# print(str(stats_arr).encode("utf-8").decode("ascii"))
-	# print(np.mean(stats_arr, axis=0))
+
 	explain_stats(np.mean(stats_arr, axis=0))
 
 
@@ -77,45 +77,52 @@ def test_model(model, x, y):
 x_train, x_test, y_train, y_test = load_data.load(one_hot=False, fill_mode='mean')
 x, y = np.concatenate((x_train, x_test)), np.concatenate((y_train, y_test))
 
-print("Our Models:")
-print("Deep Model")
-test_model(DeepKDModel(), x, y)
+# print("Our Models:")
+# print("Deep Model")
+# test_model(DeepKDModel(), x, y)
 
-print("XGBoost Model")
-test_model(XGBoostKDModel(), x, y)
+# print("XGBoost Model")
+# test_model(XGBoostKDModel(), x, y)
 
 print("")
 print("Scikit Models:")
 
-print("Logistic Regression: L2 Penalty")
-test_model(ScikitModel(LogisticRegression(penalty='l2')), x, y)
+print("Logistic Regression")
+params = {
+	'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag'],
+	# 'multi_class': ['ovr', 'multinomial'],
+	'class_weight': [None, 'balanced'],
+	'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000]#,
+	# 'penalty': ['l1', 'l2']
+}
+test_model(ScikitModel(LogisticRegression(), params), x, y)
 
-print("Logistic Regression: L1 Penalty")
-test_model(ScikitModel(LogisticRegression(penalty='l1')), x, y)
-
-print("Support Vector Classification: Linear Kernel")
-test_model(ScikitModel(SVC(kernel='linear')), x, y)
-
-print("Support Vector Classification: RBF Kernel")
-test_model(ScikitModel(SVC(kernel='rbf')), x, y)
-
-print("Support Vector Classification: Polynomial Kernel")
-test_model(ScikitModel(SVC(kernel='poly')), x, y)
-
-print("Gaussian Process Classifier")
-test_model(ScikitModel(GaussianProcessClassifier(1.0 * RBF(1.0))), x, y)
+print("Support Vector Classification")
+params = {
+	'C': np.logspace(-3, 10, 14),
+	'gamma': np.logspace(-10, 4, 15),
+	'kernel': ['linear', 'rbf', 'poly']
+}
+test_model(ScikitModel(SVC(), params), x, y)
 
 print("Random Forest")
-test_model(ScikitModel(RandomForestClassifier()), x, y)
-
-print("AdaBoost Classifier")
-test_model(ScikitModel(AdaBoostClassifier()), x, y)
-
-print("Multi-layer Perceptron")
-test_model(ScikitModel(MLPClassifier(max_iter=400)), x, y)
+params = {
+	'max_features': ['auto', 'sqrt'],
+	'n_estimators': [100, 200, 400, 600, 800, 1200, 1600, 2000],
+	'min_samples_leaf': [1, 2, 4],
+	'min_samples_split': [2, 4, 8, 16],
+	'bootstrap': [True, False],
+	'max_depth': [10, 20, 30, 40, 50, 60, 80, 100, None],
+}
+test_model(ScikitModel(RandomForestClassifier(), params), x, y)
 
 print("K Nearest Neighbors")
-test_model(ScikitModel(KNeighborsClassifier(4)), x, y)
+params = {
+	'n_neighbors':[1, 2, 3, 5, 7, 9, 13, 17, 25],
+	'leaf_size':[1,2,3,5],
+	'weights':['uniform', 'distance'],
+	'algorithm':['auto', 'ball_tree','kd_tree','brute'],
+	'n_jobs':[-1]
+	}
+test_model(ScikitModel(KNeighborsClassifier(4), params), x, y)
 
-print("Gaussian Native Bayes")
-test_model(ScikitModel(GaussianNB()), x, y)
