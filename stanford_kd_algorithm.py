a5e14bd7961d4e836bc2c63b3263d5bef51170ed
pelillian <plillian@usc.edu>
2018-07-20 21:48:03 +0200

    removed scikitmodel

--- stanford_kd_algorithm.py
+++ stanford_kd_algorithm.py
@@ -12,6 +12,7 @@
 import numpy as np
 
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.ensemble import RandomForestClassifier
 
 from sklearn.model_selection import train_test_split
 
@@ -30,50 +31,39 @@ print("STANFORD KD ALGORITHM")
 print("#BEATtheFARM --- Fight On!")
 
 ### Linear Discriminant Analysis
-lda = ScikitModel(LinearDiscriminantAnalysis(),
-	params={},
-	random_search=False,
-	scoring='roc_auc',
-	verbose=True)
-lda.train(x_train, y_train)
-lda_train_prob = lda.predict_proba(x_train)
+lda = LinearDiscriminantAnalysis()
+lda.fit(x_train, y_train)
+lda_train_proba = lda.predict_proba(x_train)[:, 1]
+lda_test_proba = lda.predict_proba(x_test)[:, 1]
+lda_test_pred = lda.predict(x_test)
 
-lda_fc_threshold, lda_kd_threshold = get_fc_kd_thresholds(lda_train_prob, y_train)
+lda_fc_threshold, lda_kd_threshold = get_fc_kd_thresholds(lda_test_pred, y_test)
 
-indeterminate_train = np.array(np.logical_and(lda_train_prob > lda_fc_threshold, lda_train_prob < lda_kd_threshold))
+indeterminate_train = np.array(np.logical_and(lda_train_proba > lda_fc_threshold, lda_train_proba < lda_kd_threshold))
+indeterminate_test = np.array(np.logical_and(lda_test_proba > lda_fc_threshold, lda_test_proba < lda_kd_threshold))
 
 x_indeterminate_train = x_train[indeterminate_train]
 y_indeterminate_train = y_train[indeterminate_train]
+x_indeterminate_test = x_test[indeterminate_test]
 
 ### Random Forest ###
-rf_params = {
-	'n_estimators': 300,
-	'max_features': 1/3
-}
-rf = ScikitModel(RandomForestClassifier(), 
-	params=rf_params,
-	random_search=True,
-	n_iter=25,
-	scoring='roc_auc',
-	verbose=True)
-rf.train(x_indeterminate_train, y_indeterminate_train)
-rf_train_prob = rf.predict_proba(x_indeterminate_train)
+rf = RandomForestClassifier(n_estimators=300, max_features=1/3)
+rf.fit(x_indeterminate_train, y_indeterminate_train)
+rf_train_proba = rf.predict_proba(x_indeterminate_train)[:, 1]
+rf_test_pred = rf.predict(x_indeterminate_test)
 
-rf_fc_threshold, rf_kd_threshold = get_fc_kd_thresholds(rf_train_prob, y_indeterminate_train)
+rf_fc_threshold, rf_kd_threshold = get_fc_kd_thresholds(rf_train_pred, y_indeterminate_train)
 
 ### Test ###
 
-lda_test_prob = lda.predict_proba(x_test)
+rf_test_proba = rf.predict_proba(x_indeterminate_test)[:, 1]
 
-indeterminate_test = np.array(np.logical_and(lda_test_prob > lda_fc_threshold, lda_test_prob < lda_kd_threshold))
-x_indeterminate_test = x_test[indeterminate_test]
+lda_fc = np.array(lda_test_proba <= lda_fc_threshold)
+lda_kd = np.array(lda_test_proba >= lda_kd_threshold)
 
-rf_test_prob = rf.predict_proba(x_indeterminate_test)
+rf_fc = np.array(rf_test_proba <= rf_fc_threshold)
+rf_kd = np.array(rf_test_proba >= rf_kd_threshold)
 
-lda_fc = np.array(lda_test_prob <= lda_fc_threshold)
-lda_kd = np.array(lda_test_prob >= lda_kd_threshold)
+final_indeterminate_test = np.array(np.logical_and(rf_test_proba > rf_fc_threshold, rf_test_proba < rf_kd_threshold))
 
-rf_fc = np.array(rf_test_prob <= rf_fc_threshold)
-rf_kd = np.array(rf_test_prob >= rf_kd_threshold)
-
-final_indeterminate_test = np.array(np.logical_and(rf_test_prob > rf_fc_threshold, rf_test_prob < rf_kd_threshold))
+print(lda_kd.shape, lda_fc.shape, rf_kd.shape, rf_fc.shape, final_indeterminate_test.shape)
